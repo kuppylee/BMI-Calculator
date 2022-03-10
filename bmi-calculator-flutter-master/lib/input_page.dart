7373ad550be2810f73cf0353ab2e7816733ff981
Mobolaji <bjkuppy@gmail.com>
2022-03-10 15:08:42 +0100

    Created an input page and the skeletal structure of the app using Expanded widget

---
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                height: 200,
+                width: 170,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0XFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),
+              ),
+              Expanded(
+                  child:Container(
+                    height: 200,
+                    width: 170,
+                    margin: EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                        color: Color(0XFF1D1E33),
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+                  ),
+              ),
+            ],
+          ),
+          ),
+          Expanded(
+            child: Container(
+              height: 200,
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                  color: Color(0XFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0)
+              ),
+            ),
+          ),
+          Expanded(child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                height: 200,
+                width: 170,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0XFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),
+              ),
+              Expanded(
+                child: Container(
+                height: 200,
+                width: 170,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0XFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),
+              ),
+            ],
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}

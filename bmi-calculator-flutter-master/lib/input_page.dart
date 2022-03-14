0349d4adb410156ce7509a352ea4fcf4eee77121
Mobolaji <bjkuppy@gmail.com>
2022-03-14 21:37:05 +0100

    Added functionality to the button widget created

--- bmi-calculator-flutter-master/lib/input_page.dart
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -120,18 +120,22 @@ class _InputPageState extends State<InputPage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          FloatingActionButton(onPressed: (){},
-                            backgroundColor: Color(0xFF4C4F5E),
-                            child: Icon(Icons.add,
-                              color: Colors.white,
-                            ),
+                          RoundIconButton(
+                            icon: FontAwesomeIcons.minus,
+                            onPressed: (){
+                              setState(() {
+                                weight--;
+                              });
+                            },
                           ),
                           SizedBox(width: 10.0,),
-                          FloatingActionButton(onPressed: (){},
-                            backgroundColor: Color(0xFF4C4F5E),
-                            child: Icon(Icons.add,
-                              color: Colors.white,
-                            ),
+                          RoundIconButton(
+                            icon: FontAwesomeIcons.plus,
+                            onPressed: (){
+                              setState(() {
+                                weight++;
+                              });
+                            },
                           ),
                         ],
                       ),
@@ -159,12 +163,22 @@ class _InputPageState extends State<InputPage> {
 
 class RoundIconButton extends StatelessWidget {
 
+  RoundIconButton({@required this.icon, @required this.onPressed});
+  final IconData icon;
+  final Function onPressed;
+
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-      onPressed: (){},
+      child: Icon(icon),
+      onPressed: onPressed,
       shape: CircleBorder(),
       fillColor: Color(0xFF4C4F5E),
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      elevation: 0.0,
 
     );
   }

46c0e0f6d90308b1e2e73aaf31f8abb35809f7f8
Mobolaji <bjkuppy@gmail.com>
2022-03-14 06:05:46 +0100

    Customized the slider widget to fit the display i needed it for

--- bmi-calculator-flutter-master/lib/input_page.dart
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -78,19 +78,26 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      activeColor: Color(0xFFEB1555),
-                      inactiveColor: Color(0xFF8D8E98),
-                      onChanged: (double newValue){
-                        print(newValue);
-                        setState(() {
-                          height = newValue.toInt();
-                        });
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape: RoundSliderThumbShape(enabledThumbRadius: 30.0),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x1FEB1555),
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                    ),
+                    child: Slider(
+                        value: height.toDouble(),
+                        min: 120.0,
+                        max: 220.0,
+                        onChanged: (double newValue){
+                          setState(() {
+                            height = newValue.toInt();
+                          });
 
-                      }),
+                        }),
+                  ),
                 ],
               ),
             ),
